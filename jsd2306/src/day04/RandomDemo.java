c2353ad94f806828e239ff5220f2182a3636b5b1
Beeeee-really <138751506+Beeeee-really@users.noreply.github.com>
2023-07-07 11:55:33 +0800

    Delete jsd2306 directory

--- jsd2306/src/day04/RandomDemo.java
+++
@@ -1,9 +0,0 @@
-package day04;
-
-public class RandomDemo {
-    public static void main(String[] args) {
-        System.out.println(Math.random());// 0.0 到0.99999999999999
-
-        System.out.println((int)(Math.random() * 1000));//1 到1000
-    }
-}
